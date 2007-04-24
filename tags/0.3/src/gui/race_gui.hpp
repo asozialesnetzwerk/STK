48b7c0e0fc2a74f45b0cd52ab6fcb4f728ea79c8
coz <coz@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2007-04-24 17:20:55 +0000

    Made of copy of the trunk into the 0.3 tag
    git-svn-id: https://svn.code.sf.net/p/supertuxkart/code@1020 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ tags/0.3/src/gui/race_gui.hpp
@@ -0,0 +1,111 @@
+//  $Id$
+//
+//  SuperTuxKart - a fun racing game with go-kart
+//  Copyright (C) 2004-2005 Steve Baker <sjbaker1@airmail.net>
+//  Copyright (C) 2006 Joerg Henrichs, SuperTuxKart-Team, Steve Baker
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef HEADER_RACEGUI_H
+#define HEADER_RACEGUI_H
+
+#include "base_gui.hpp"
+#include "material.hpp"
+#include "player_kart.hpp"
+#include "player.hpp"
+
+// TODO: Fix this.
+#define MAX_ID0 512
+#define MAX_ID1 16
+#define MAX_ID2 2
+
+typedef struct
+{
+    PlayerKart *kart;
+    KartActions action;
+}
+Entry;
+
+class RaceSetup;
+
+class RaceGUI: public BaseGUI
+{
+    // A mapping for the assigned keys (like fire, ...) to
+    // the kart which is using them
+    Entry m_input_map[IT_LAST+1][MAX_ID0][MAX_ID1][MAX_ID2];
+
+    float       m_time_of_leader;
+    int         m_lap_leader;
+
+public:
+    RaceGUI();
+    ~RaceGUI();
+    void update(float dt);
+    void select() {}
+    void input(InputType type, int id0, int id1, int id2, int value);
+    void handleKartAction(KartActions ka, int value);
+
+private:
+    ulClock   m_fps_timer;
+    int       m_fps_counter;
+    char      m_fps_string[10];
+    double    m_time_left ;
+    char*     m_pos_string [11];
+    Material* m_steering_wheel_icon;
+    Material* m_speed_back_icon;
+    Material* m_speed_fore_icon;
+
+    /* Display informat on screen */
+    void drawStatusText        (const RaceSetup& raceSetup, const float dt);
+    void drawEnergyMeter       (Kart *player_kart,
+                                int   offset_x, int   offset_y,
+                                float ratio_x,  float ratio_y  );
+    void drawCollectableIcons  (Kart* player_kart,
+                                int   offset_x, int   offset_y,
+                                float ratio_x,  float ratio_y  );
+    void drawEmergencyText     (Kart* player_kart,
+                                int   offset_x, int   offset_y,
+                                float ratio_x,  float ratio_y  );
+    void UpdateKeyboardMappings();
+    void putEntry(PlayerKart *kart, KartActions ka);
+    bool handleInput(InputType type, int id0, int id1, int id2, int value,
+                     bool isKeyboard=false);
+    void inputKeyboard(int key, int pressed);
+    void drawPlayerIcons       ();
+    void oldDrawPlayerIcons    ();
+    void drawGameOverText      (const float dt);
+    void drawMap               ();
+    void drawTimer             ();
+    void drawFPS               ();
+
+    /* Text drawing */
+    /** Draw text to screen.
+        scale_x and scale_y could be used to a simple resize (e.g. for multiplayer
+        split screens, though, currently, we reduce fonts size to half).        */
+    void drawSteering             (Kart* kart, int offset_x, int offset_y,
+                                   float ratio_x, float ratio_y           );
+    void drawPosition             (Kart* kart, int offset_x, int offset_y,
+                                   float ratio_x, float ratio_y           );
+    void drawSpeed                (Kart* kart, int offset_x, int offset_y,
+                                   float ratio_x, float ratio_y           );
+    void drawLap                  (Kart* kart, int offset_x, int offset_y,
+                                   float ratio_x, float ratio_y           );
+#if 0
+    void drawTexture              (const GLuint texture, int w, int h, int red,
+                                   int green, int blue, int x, int y);
+#endif
+};
+
+#endif
---
+++ tags/0.3/src/start_screen.cpp
@@ -0,0 +1,134 @@
+//  $Id$
+//
+//  SuperTuxKart - a fun racing game with go-kart
+//  Copyright (C) 2006 SuperTuxKartTeam
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#if !defined(WIN32) && !defined(__CYGWIN__)
+#  include <unistd.h>   // for usleep
+#endif
+#include <SDL/SDL.h>
+
+#include "loader.hpp"
+#include "race_manager.hpp"
+#include "start_screen.hpp"
+#include "material.hpp"
+#include "material_manager.hpp"
+#include "gui/menu_manager.hpp"
+#include "sound_manager.hpp"
+
+StartScreen* startScreen = NULL;
+
+StartScreen::StartScreen() : m_intro_material(NULL)
+{
+    menu_manager->switchToMainMenu();
+    installMaterial();
+}
+
+//-----------------------------------------------------------------------------
+StartScreen::~StartScreen()
+{
+    m_intro_material= NULL;
+}   // ~StartScreen
+
+//-----------------------------------------------------------------------------
+void StartScreen::update()
+{
+    //Setup for boring 2D rendering
+    glMatrixMode   ( GL_PROJECTION ) ;
+    glLoadIdentity () ;
+    glMatrixMode   ( GL_MODELVIEW ) ;
+    glLoadIdentity () ;
+    glDisable      ( GL_DEPTH_TEST ) ;
+    glDisable      ( GL_LIGHTING   ) ;
+    glDisable      ( GL_FOG        ) ;
+    glDisable      ( GL_CULL_FACE  ) ;
+    glDisable      ( GL_ALPHA_TEST ) ;
+
+    // On at least one platform the X server apparently gets overloaded
+    // by the large texture, resulting in buffering of key events. This
+    // results in the menu being very unresponsive/slow - it can sometimes
+    // take (say) half a second before the menu reacts to a pressed key.
+    // This is caused by X buffering the key events, delivering them
+    // later (and sometimes even several at the same frame). This issue
+    // could either be solved by a lazy drawing of the background picture
+    // (i.e. draw the background only if something has changed) - which is
+    // a lot of implementation work ... or by sleeping for a little while,
+    // which apparently reduces the load for the X server, so that no
+    // buffering is done --> all key events are handled in time.
+#if !defined(WIN32) && !defined(__CYGWIN__)
+    usleep(2000);
+#endif
+    //Draw the splash screen
+    m_intro_material->getState()->force();
+
+    glBegin ( GL_QUADS ) ;
+    glColor3f   (1, 1, 1 ) ;
+    glTexCoord2f(0, 0); glVertex2i(-1, -1);
+    glTexCoord2f(1, 0); glVertex2i( 1, -1);
+    glTexCoord2f(1, 1); glVertex2i( 1,  1);
+    glTexCoord2f(0, 1); glVertex2i(-1,  1);
+    glEnd () ;
+
+    glFlush();
+    menu_manager->update();
+    sound_manager->update();
+
+    // Swapbuffers - and off we go again...
+    SDL_GL_SwapBuffers();
+}   // update
+
+//-----------------------------------------------------------------------------
+void StartScreen::removeTextures()
+{
+    // The current context (within plib) has a pointer to the last applied
+    // texture, which means that the background image will not be deleted
+    // as part of material_manager->reInit. To fix this, we load a NULL
+    // texture into the current context, which decreases the ref counter
+    // of the background image, and allows it to be deleted.
+    // Doing it this way is not really nice, so any better way is appreciated :)
+    _ssgCurrentContext->getState()->setTexture((ssgTexture*)NULL);
+
+}   // removeTextures
+
+// -----------------------------------------------------------------------------
+void StartScreen::installMaterial()
+{
+    /* Make a simplestate for the title screen texture */
+    m_intro_material = material_manager->getMaterial("st_title_screen.rgb");
+}   // installMaterial
+
+//-----------------------------------------------------------------------------
+void StartScreen::switchToGame()
+{
+    // As soon as the game is started, display the background picture only
+    // so that the user gets feedback that his selection was done.
+    m_intro_material->getState()->force();
+
+    glBegin ( GL_QUADS ) ;
+    glColor3f   (1, 1, 1 ) ;
+    glTexCoord2f(0, 0); glVertex2i(-1, -1);
+    glTexCoord2f(1, 0); glVertex2i( 1, -1);
+    glTexCoord2f(1, 1); glVertex2i( 1,  1);
+    glTexCoord2f(0, 1); glVertex2i(-1,  1);
+    glEnd () ;
+
+    glFlush();
+    SDL_GL_SwapBuffers();
+    race_manager->start();
+}   // switchToGame
+
+/* EOF */
