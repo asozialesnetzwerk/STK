c9c1ee64a941a89ec22552772801923c11f7bbe8
coz <coz@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2007-07-31 02:16:00 +0000

    Added 0.3 tag.
    git-svn-id: https://svn.code.sf.net/p/supertuxkart/code@1192 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ tags/supertuxkart/0.3/src/empty_screen.cpp
@@ -0,0 +1,43 @@
+//  $Id$
+//
+//  SuperTuxKart - a fun racing game with go-kart
+//  Copyright (C) 2006 SuperTuxKart-Team
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include <SDL/SDL.h>
+
+#include "empty_screen.hpp"
+#include "start_screen.hpp"
+#include "screen_manager.hpp"
+#include "gui/menu_manager.hpp"
+
+EmptyScreen::EmptyScreen()
+{}
+
+//-----------------------------------------------------------------------------
+EmptyScreen::~EmptyScreen()
+{}
+
+//-----------------------------------------------------------------------------
+void
+EmptyScreen::update()
+{
+    menu_manager->update();
+
+    SDL_GL_SwapBuffers();
+}
+
+/* EOF */
