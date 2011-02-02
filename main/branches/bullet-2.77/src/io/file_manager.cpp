99fb321c837d978360bae2f95afa7054c93b1340
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-02-02 23:12:32 +0000

    Included compilation fixes and updates from trunk (also fixed removeDirectory on windows).
    git-svn-id: https://svn.code.sf.net/p/supertuxkart/code@7583 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- main/branches/bullet-2.77/src/io/file_manager.cpp
+++ main/branches/bullet-2.77/src/io/file_manager.cpp
@@ -32,9 +32,9 @@
 #  include <sys/stat.h>
 #  include <sys/types.h>
 // For RemoveDirectory
-#  include <Windows.h>
 #else
 #  include <direct.h>
+#  include <Windows.h>
 #endif
 
 /*Needed by the remove directory function */
@@ -751,8 +751,7 @@ bool FileManager::removeDirectory(const std::string &name) const
         }
     }
 #ifdef WIN32
-//FIXME    return RemoveDirectory(name.c_str());
-        return false;
+        return RemoveDirectory(name.c_str())==TRUE;
 #else
     return remove(name.c_str())==0;
 #endif
