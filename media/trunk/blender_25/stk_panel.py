1d489182478111adbf66623615f63570115968ba
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-05-07 21:02:37 +0000

    Make color picker easier to use
    git-svn-id: https://svn.code.sf.net/p/supertuxkart/code@8527 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- media/trunk/blender_25/stk_panel.py
+++ media/trunk/blender_25/stk_panel.py
@@ -188,7 +188,7 @@ class StkColorProperty(StkProperty):
                 box = layout.box()
                 row = box.row()
                 try:
-                    row.template_color_wheel(self, "temp_color", value_slider=True)
+                    row.template_color_wheel(self, "temp_color", value_slider=True, cubic=False)
                 except Exception as ex:
                     import sys
                     print("Except :(", type(ex), ex, "{",ex.args,"}")
