d3c5182f932a1467a6a7abcd2c6df44ff6999735
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2012-04-22 22:43:17 +0000

    Add translation to curve points
    git-svn-id: https://svn.code.sf.net/p/supertuxkart/code@11137 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- media/trunk/blender_25/stk_track.py
+++ media/trunk/blender_25/stk_track.py
@@ -732,6 +732,7 @@ class TrackExport:
     # --------------------------------------------------------------------------
     
     def writeBezierCurve(self, f, curve, speed):
+        translation = curve.location
         matrix = curve.rotation_euler.to_matrix()
         if len(curve.data.splines) > 1:
             log_warning(curve.name + " contains multiple curves, will only export the first one")
@@ -741,9 +742,9 @@ class TrackExport:
             log_warning(curve.name + " should be a bezier curve, not a " + curve.data.splines[0].type)
         else:
             for pt in curve.data.splines[0].bezier_points:
-                v0 = pt.handle_left*matrix
-                v1 = pt.co*matrix
-                v2 = pt.handle_right*matrix
+                v0 = pt.handle_left*matrix + translation
+                v1 = pt.co*matrix + translation
+                v2 = pt.handle_right*matrix + translation
                 f.write("      <point c=\"%f %f %f\" h1=\"%f %f %f\" h2=\"%f %f %f\" />\n"% \
                         ( v1[0],v1[2],v1[1],
                           v0[0],v0[2],v0[1],
