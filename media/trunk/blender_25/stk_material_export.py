44c796ed735d12e4f4c605a4899858f06b4970fd
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2013-06-28 23:59:49 +0000

    Add physics-only material flag
    git-svn-id: https://svn.code.sf.net/p/supertuxkart/code@13024 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- media/trunk/blender_25/stk_material_export.py
+++ media/trunk/blender_25/stk_material_export.py
@@ -106,6 +106,7 @@ def writeMaterialsFile(sPath):
            'mask'                  : ("", None),
            'normal_map'            : ("", ('graphical_effect','normal_map')),
            'normal_light_map'      : ("", ('graphical_effect','normal_map')),
+           'physics_only'          : ("N", None),
            'reset'                 : ("N", None),
            'surface'               : ("N", None),
            'high_adhesion'         : ('false', None),
