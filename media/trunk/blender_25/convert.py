37cb69b1d365cb6e352f53c2cf661f0cb6a263df
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2014-10-08 23:10:15 +0000

    Improve migration script
    git-svn-id: https://svn.code.sf.net/p/supertuxkart/code@15682 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- media/trunk/blender_25/convert.py
+++ media/trunk/blender_25/convert.py
@@ -4,17 +4,22 @@ print('=======================================')
 
 for img in bpy.data.images:
     print(img.name)
+    is_alpha = False
     if 'compositing' in img:
         #print('    compositing = ', img['compositing'])
         
         if img['compositing'] == 'blend':
             img['shader'] = 'alphablend'
+            is_alpha = True
         if img['compositing'] == 'test':
             img['shader'] = 'alphatest'
+            is_alpha = True
         if img['compositing'] == 'additive':
             img['shader'] = 'additive'
+            is_alpha = True
         if img['compositing'] == 'coverage':
             img['shader'] = 'alphatest'
+            is_alpha = True
         
     if 'graphical_effect' in img:
         #print('    graphical_effect = ', img['graphical_effect'])
@@ -23,6 +28,7 @@ for img in bpy.data.images:
             img['shader'] = 'bubble'
         if img['graphical_effect'] == 'grass':
             img['shader'] = 'grass'
+            is_alpha = True
         if img['graphical_effect'] == 'spheremap':
             img['shader'] = 'spheremap'
         if img['graphical_effect'] == 'splatting':
@@ -30,7 +36,7 @@ for img in bpy.data.images:
         if img['graphical_effect'] == 'water_shader':
             img['shader'] = 'water_shader'
             
-    if 'light' in img and img['light'] == 'false':
+    if 'light' in img and img['light'] == 'false' and not is_alpha:
         img['shader'] = 'unlit'
         
         
